7681cec9ffddf25713e056b75245f7c5e26511e7
PierogiHusarianGaming <91559196+PierogiHusarianGaming@users.noreply.github.com>
2022-10-05 20:05:18 +0200

    Add project files.

---
+++ OOP_PanelLogowania/Form1.cs
@@ -0,0 +1,52 @@
+﻿using OOP_PanelLogowania.Klasy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_PanelLogowania
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //string login = "admin";
+            //string haslo = "admin";
+
+
+            var user = new Uzytkownik();
+            var user2 = new Uzytkownik();
+
+            user.Login = "giganci";
+            user.Haslo = "giganci123";
+            user = new Uzytkownik();
+            
+
+            if (user.Login == textBox1.Text && user.Haslo == textBox2.Text)
+            {
+                MessageBox.Show($"Zalogowano poprawnie. {user.PrzedstawSie()}",
+                                "Komunikat",
+                                 MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show($"Login lub haslo niepoprawne. Podano login: {textBox1.Text}, a szukamy loginu {user.Login}",
+                               "Błąd",
+                                MessageBoxButtons.OK);
+            }
+
+          
+
+        }
+    }
+}
---
+++ OOP_PanelLogowania/Klasy/Uzytkownik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_PanelLogowania.Klasy
+{
+    public class Uzytkownik
+    {
+        public string Login { get; set; }
+        public string Haslo { get; set; }
+        public string Wiek { get; set; }
+        public string Email { get; set; }
+        
+
+        public Uzytkownik()
+        {
+            Login = "admin";
+            Haslo = "admin";
+        }
+       
+        public string PrzedstawSie()
+        {
+            var tekst = $"Witaj {Login}";
+            return tekst;
+        }
+
+
+
+    }
+}
